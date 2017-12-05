f56fb779d14d8ccab13eccdc0d37ceab4c364555
Beomjin Ahn <expect017@naver.com>
2017-12-05 22:28:35 +0900

    bouncing cube

--- Assets/PythonEX7.py
+++ Assets/PythonEX7.py
@@ -3,27 +3,34 @@ import random
 sys.path.append('C:\\COSE433\\Assets\\Library\\')
 import pymunk
 
+interval = 0
+
 class PythonEX7(Actor.Actor):
 
     space = None
     ball = None
+    grounds = None
 
     def __init__(self):
         print("__init__ is called")
 
         return
 
+    def danuri_coor(self, coor):
+        return (coor-300.0)/50.0
+
     def add_ball(self):
         global space, ball
-        mass = 1
-        radius = 14
-        moment = pymunk.moment_for_circle(mass, 0, radius)  # 1
-        body = pymunk.Body(mass, moment)  # 2
-        x = random.randrange(120, 380)
-#        body.position = x, 550  # 3
+        mass = 0.1
+        radius = 1
+        moment = pymunk.moment_for_circle(mass, 0, radius)
+        body = pymunk.Body(mass, moment)
         body.position = 0, 0
-        shape = pymunk.Circle(body, radius)  # 4
-        space.add(body, shape)  # 5
+#        body.gravity = (-9.0, 0.0)
+        shape = pymunk.Circle(body, radius)
+        shape.elasticity = 0.95
+        shape.friction = 0.9
+        space.add(body, shape)
         return shape
 
 #    def draw_ball(self, screen, ball):
@@ -31,16 +38,30 @@ class PythonEX7(Actor.Actor):
 #        pygame.draw.circle(screen, (0, 0, 255), p, int(self.ball.radius), 2)
 
     def OnCreate(self, uid):
-        global space, ball
+        global space, ball, grounds
 
         print("OnCreate is called")
         print(sys.version)
+
         self.cubeContainer = GetWorldContainer().FindContainer("Cube")
+        self.groundContainer = GetWorldContainer().FindContainer("Ground")
+
+        self.cubeTransformGroup = self.cubeContainer.FindComponentByType("TransformGroup")
+        self.groundTransformGroup = self.groundContainer.FindComponentByType("TransformGroup")
+        groundPosition = self.groundTransformGroup.GetPosition()
+        groundScale = self.groundTransformGroup.GetScale()
 
         space = pymunk.Space()
-        space.gravity = (0.0, -1.0)
+        space.gravity = (0.0, -9.0)
 
         ball = self.add_ball()
+
+        static_body = space.static_body
+        grounds = [pymunk.Segment(static_body, (groundPosition.x-groundScale.x/2.0, groundPosition.y), (groundPosition.x+groundScale.x/2.0, groundPosition.y), 0.0)]
+        for line in grounds:
+            line.elasticity = 0.95
+            line.friction = 0.9
+        space.add(grounds)
 
         return 0
 
@@ -54,17 +75,25 @@ class PythonEX7(Actor.Actor):
         return 0
 
     def Update(self):
-        global space, ball
+        global interval, space, ball, grounds
+        if interval == 0:
 
-        cubeTransformGroup = self.cubeContainer.FindComponentByType("TransformGroup")
-        cubePosition = cubeTransformGroup.GetPosition()
-        cubePosition.x = ball.body.position.x
-        cubePosition.y = ball.body.position.y
-        print (cubePosition.x, cubePosition.y)
-        cubeTransformGroup.SetPosition(cubePosition)
 
-        space.step(1/50.0)
+            #groundPosition = self.groundTransformGroup.GetPosition()
+            cubePosition = self.cubeTransformGroup.GetPosition()
+            cubePosition.x = ball.body.position.x
+            cubePosition.y = ball.body.position.y
+            #groundPosition.x = ground.body.position.x
+            #groundPosition.y = ground.body.position.y
+            self.cubeTransformGroup.SetPosition(cubePosition)
+            #self.groundTransformGroup.SetPosition(groundPosition)
 
+            print ('cubePosition : ', cubePosition.x, cubePosition.y)
+            #print ('groundPosition : ', groundPosition.x, groundPosition.y)
+
+            space.step(1/60.0)
+            interval = 1
+        interval-=1
         return
 
     def OnMessage(self, msg, number, Vector4_lparm, Vector4_wparam):
